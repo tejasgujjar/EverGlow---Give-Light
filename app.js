298c77bb7404f43aedea9be8b50a48998d08ecd6
yashasm1 <yashasm1991@gmail.com>
2017-10-29 04:46:37 -0700

    Adding globe angular

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
 
 app.get('/new-page', function(req, res) {
@@ -69,4 +70,3 @@ mongoose.connect(mongoSessionConnectURL, function(){
   console.log("Connected correctly to server.");
   db.close();
 });*/
-
