7822ece3a5c826191639679f721e5079c55c96b2
yashasm1 <yashasm1991@gmail.com>
2017-10-28 22:26:10 -0700

    Added search all get request

--- routes/volunteers.js
+++ routes/volunteers.js
@@ -1,7 +1,7 @@
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var mongoSessionConnectURL = "mongodb://root:root@ds229435.mlab.com:29435/user_db";
-
+//var prettyjson = require('prettyjson');
 
 var checkstatus = function(req,res){
  console.log("lets check status");
@@ -14,12 +14,44 @@ var checkstatus = function(req,res){
 
 var searchall = function(req,res){
   console.log("Inside searchAll");
+  var docs = [];
   MongoClient.connect(mongoSessionConnectURL, function(err, db) {
     assert.equal(null, err);
     console.log("Connected correctly to server.");
-    db.close();
+    myCollection = db.collection('user_db');
+    //var cursor = myCollection.find();
+    /*myCollection.find({"Name": "Elaf"},function(err, doc) {
+         if (err) {
+           console.log('Error: ', err);
+         } else {
+           console.log(doc);
+
+         }
+
+    });*/
+    myCollection.find()
+		.toArray(function(err,ans1){
+
+            if(err) {
+
+            	console.log(err);
+
+            	//callback(true);
+            	return;
+            	}
+              //console.log("returning from query",ans1);
+              res
+              .status(200)
+              .json({"test":ans1});
+               db.close();
+
+    });
+
+
+
   });
 };
 
 
 exports.checkstatus = checkstatus;
+exports.searchall = searchall;
