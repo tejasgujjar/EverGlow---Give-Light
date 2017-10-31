ef3c11f41b2c5a1bb18c0202b980c907d93f2d16
yashasm1 <yashasm1991@gmail.com>
2017-10-30 23:33:04 -0700

    added query

--- routes/volunteers.js
+++ routes/volunteers.js
@@ -18,6 +18,7 @@ var checkstatus = function(req,res){
 var searchall = function(req,res){
   console.log("Inside searchAll");
   console.log(req.query.search);
+  searchString = req.query.search.toLowerCase();
 
   MongoClient.connect(mongoSessionConnectURL, function(err, db) {
     assert.equal(null, err);
@@ -25,9 +26,10 @@ var searchall = function(req,res){
     myCollection = db.collection('user_db');
 
     var searchString = req.query.search;
-
+//{awards: {$elemMatch: {award:'National Medal', year:1975}}}
     myCollection.find({$or: [ { "City": {'$regex': searchString,$options:'i'}}, { "Name": {'$regex': searchString,$options:'i'} },{ "Contact Number": {'$regex': searchString,$options:'i'} }]})
-		.toArray(function(err,ans1){
+    //myCollection.find()
+    .toArray(function(err,ans1){
 
             if(err) {
             	console.log(err);
@@ -35,6 +37,7 @@ var searchall = function(req,res){
             	}
               for(var obj in ans1){
                  //console.log(ans1[obj]);
+                 //if(ans1[obj].City.toLowerCase() == )
                  var all_skills = [];
                  if(ans1[obj].T == "Y") all_skills.push("Governing");
                  if(ans1[obj].E == "Y") all_skills.push("Operations");
@@ -71,6 +74,18 @@ var searchhome = function(req,res){
             	console.log(err);
             	return;
             	}
+              /*for(var obj in ans1){
+                 //console.log(ans1[obj]);
+                 var all_skills = [];
+                 if(ans1[obj].T == "Y") all_skills.push("Governing");
+                 if(ans1[obj].E == "Y") all_skills.push("Operations");
+                 if(ans1[obj].G == "Y") all_skills.push("Marketing");
+                 if(ans1[obj].D == "Y") all_skills.push("Human Resources");
+                 if(ans1[obj].M == "Y") all_skills.push("Technology");
+                 if(ans1[obj].W == "Y") all_skills.push("Programs/Outreach");
+                 if(ans1[obj].P == "Y") all_skills.push("Global Homes");
+                 ans1[obj].all_skills = all_skills;
+              }*/
 
               res
               .status(200)
@@ -97,6 +112,18 @@ var searchone = function(req,res){
             	console.log(err);
             	return;
             	}
+              for(var obj in ans1){
+                 //console.log(ans1[obj]);
+                 var all_skills = [];
+                 if(ans1[obj].T == "Y") all_skills.push("Governing");
+                 if(ans1[obj].E == "Y") all_skills.push("Operations");
+                 if(ans1[obj].G == "Y") all_skills.push("Marketing");
+                 if(ans1[obj].D == "Y") all_skills.push("Human Resources");
+                 if(ans1[obj].M == "Y") all_skills.push("Technology");
+                 if(ans1[obj].W == "Y") all_skills.push("Programs/Outreach");
+                 if(ans1[obj].P == "Y") all_skills.push("Global Homes");
+                 ans1[obj].all_skills = all_skills;
+              }
 
               res
               .status(200)
@@ -105,7 +132,57 @@ var searchone = function(req,res){
     });
   });
 };
+/*
+var temp = function(req,res){
+  MongoClient.connect(mongoSessionConnectURL, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server.");
+    myCollection = db.collection('user_db');
+    //var o_id = new mongo.ObjectID(req.query.id);
+    //{"_id":o_id}
+    myCollection.find()
+    .toArray(function(err,ans1){
 
+            if(err) {
+              console.log(err);
+              return;
+              }
+              for(var obj in ans1){
+                 //console.log(ans1[obj]);
+
+                 var all_skills = [];
+                 if(ans1[obj].T == "Y") all_skills.push("Governing");
+                 if(ans1[obj].E == "Y") all_skills.push("Operations");
+                 if(ans1[obj].G == "Y") all_skills.push("Marketing");
+                 if(ans1[obj].D == "Y") all_skills.push("Human Resources");
+                 if(ans1[obj].M == "Y") all_skills.push("Technology");
+                 if(ans1[obj].W == "Y") all_skills.push("Programs/Outreach");
+                 if(ans1[obj].P == "Y") all_skills.push("Global Homes");
+                 ans1[obj].all_skills = all_skills;
+                 //MongoClient.connect(mongoSessionConnectURL, function(err, dbnew) {
+                 //updateCollection = dbnew.collection('user_db');
+                 var o_id = new mongo.ObjectID(ans1[obj]._id);
+                 //updateCollection.update({"_id":o_id},ans1[obj]);
+                 //dbnew.collection("user_db").updateOne({"_id":o_id}, {$set : {"all_skills":all_skills}}, function(err, res) {
+                 db.collection("user_db").updateOne({"_id":o_id}, {$set : {"all_skills":all_skills}}, function(err, res) {
+                   if (err) throw err;
+                   console.log("1 document updated"+res);
+                   //dbnew.close();
+                 });
+
+               //});
+                 //break;
+              }
+
+              res
+              .status(200)
+              .json({"test":"test"});
+               db.close();
+    });
+  });*/
+
+
+//exports.temp = temp;
 exports.checkstatus = checkstatus;
 exports.searchall = searchall;
 exports.searchone = searchone;
