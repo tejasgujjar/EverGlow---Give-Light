201a81291b059f58afe4e7cb4a8e687f347660a3
yashasm1 <yashasm1991@gmail.com>
2017-10-31 20:49:59 -0700

    Near me

--- routes/near_me.js
+++ routes/near_me.js
@@ -3,6 +3,7 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var mongoSessionConnectURL = "mongodb://root:root@ds229435.mlab.com:29435/user_db";
 var geolib = require('geolib');
+var mongo = require('mongodb');
 
 
 
@@ -11,21 +12,96 @@ var options = {
 
  // Optional depending on the providers
   httpAdapter: 'https', // Default
-  apiKey: 'AIzaSyCMmLAeJIEBZ_Ckajl3IGGPiTfXSKAx-do', // for Mapquest, OpenCage, Google Premier
+  //apiKey: 'AIzaSyCMmLAeJIEBZ_Ckajl3IGGPiTfXSKAx-do', // for Mapquest, OpenCage, Google Premier
+  apiKey: 'AIzaSyBYvmEYbY9AfH28f70hDwGFnQXcqJGaevQ',
   formatter: null         // 'gpx', 'string', ...
 };
 
 
 
 var testnear = function(req,res){
-  var geocoder = NodeGeocoder(options);
-  geocoder.geocode('San Jose', function(err, res) {
-  console.log(res);
+
+
+MongoClient.connect(mongoSessionConnectURL, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  myCollection = db.collection('user_db');
+
+  myCollection.find()
+  .toArray(function(err,ans1){
+
+          if(err) {
+            console.log(err);
+            return;
+            }
+
+
+ var ind = 0;
+            while(ind < 5){
+              //for(var j in ans1[i].all_skills){
+              console.log(ind);
+
+                var geocoder = NodeGeocoder(options);
+                console.log(ans1[ind].City);
+                geocoder.geocode(ans1[ind].City, function(err, res) {
+                  console.log(res);
+
+                  if(typeof res != 'undefined'){
+                    //console.log("========="+ans1[i]._id);
+                    myCollection = db.collection('user_db');
+                    MongoClient.connect(mongoSessionConnectURL, function(err, dbnew) {
+                    var o_id = new mongo.ObjectID(ans1[ind]._id);
+                    //console.log("res.latitude"+res[0].latitude);
+                    //console.log("res.longitude"+res[0].longitude);
+                    //,"longitude":res[0].longitude
+                    console.log(ind);
+                    console.log("id"+ans1[ind]._id);
+                    //console.log("lattt"+res[0].latitude);
+                    /*dbnew.collection("user_db").update({"_id":o_id},
+                    {$set : {"latitude":res[0].latitude}}, function(err, data) {
+                      if (err) throw err;
+                      console.log("1 document updated"+data);
+                      dbnew.close();
+                    });*/
+
+
+                    /*dbnew.collection('user_db').findAndModify({
+                        query: {"_id":o_id},
+                        update: {$set : {"latitude":res[0].latitude}},
+                        new: true,
+                        upsert: false
+                        },
+                        function(err, doc){
+                            console.log(doc);
+                    });*/
+
+
+                  });
+
+                }else{
+                    console.log("+++++++++");
+                  }
+
+              });
+
+
+              //}
+              ind++;
+            }
+
+
+
+            res
+            .status(200)
+            .json({"test":ans1});
+             db.close();
+  });
 });
 
-res
-              .status(200)
-              .json({"field_details":""});
+
+// res
+//               .status(200)
+//               .json({"field_details":""});
 
 
 }
